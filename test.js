d035c03284585fb10f6e996c306f63715e6ea934
yunjiNam <yj607252@gmail.com>
2020-05-15 14:33:11 +0900

    edit test file

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 const makeNum = num => {
-
+    let result = [];
+    for (let i = 0; i < num.length; i++) {
+       if ( num[i]% 2 == 0 ) {
+          result.push(num[i]);
+       }
+    }
+    return result;
 }
+
+makeNum([1,2,3,4,5,6]);
+// [2,4,6] 출력
